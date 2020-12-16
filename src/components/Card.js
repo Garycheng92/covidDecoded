a2eeb8cd1750c4d795fe07b0367046aa55e2cab8
garycheng92 <garycheng292@gmail.com>
2020-12-16 13:36:17 -0500

    Added event handlers

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+
+function updateChart(e) {
+	e.preventDefault();
+	console.log('link clicked')
+}
+
 const Card = (props) => {
 	return(
+		<a href='' onClick={updateChart}>
 		<div className="card">
 			{props.children}
 		</div>
+		</a>
 	)
 }
 
